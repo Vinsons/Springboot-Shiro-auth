0d976678461266b5d8e067889ca11451160a4d86
Huangwencong <wenc.huang@febc.com.cn>
2019-11-25 15:13:31 +0800

    wencong
    初始化

---
+++ Springboot-Shiro-Auth/src/main/java/com/shiro/auth/pojo/User.java
@@ -0,0 +1,16 @@
+package com.shiro.auth.pojo;
+
+import lombok.Data;
+
+/**
+ * @ClassName User
+ * @Description
+ * @Author 黄文聪
+ * @Date 2019-11-25 15:13
+ * @Version 1.0
+ **/
+@Data
+public class User {
+    private String userName;
+    private String pwd;
+}
